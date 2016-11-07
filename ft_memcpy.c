23aa5ab00aca2bf726bb6550b6d916bd6c93c45b
Arthur GASPARD <agaspard@e1r8p17.42.fr>
2016-11-07 15:21:28 +0100

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: agaspard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 14:12:57 by agaspard          #+#    #+#             */
-/*   Updated: 2016/11/07 14:44:29 by agaspard         ###   ########.fr       */
+/*   Updated: 2016/11/07 15:21:07 by agaspard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 void			*ft_memcpy(void *dst, const void *src, size_t n)
 {
+	int i;
 	char		*ptrdst;
 	const char	*ptrsrc;
 
+	i = 0;
 	ptrdst = dst;
 	ptrsrc = src;
-	while (n--)
+	while (i < n)
+	{
 		*ptrdst++ = *ptrsrc++;
+		i++;
+	}
 	return (dst);
 }
