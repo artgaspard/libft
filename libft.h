d6f23ef24050df2bea9880e68241bb16958fd138
Arthur GASPARD <agaspard@e1r8p15.42.fr>
2016-11-08 11:24:58 +0100

    maj

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: agaspard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 18:39:34 by agaspard          #+#    #+#             */
-/*   Updated: 2016/11/07 15:38:03 by agaspard         ###   ########.fr       */
+/*   Updated: 2016/11/08 11:24:38 by agaspard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ typedef struct	s_list
 void		*ft_memset(void *b, int c, size_t len);
 void		ft_bzero(void *s, size_t n);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
-void		*ft_memccpy(void *dst, const void src, int c, size_t n);
+void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
 //void		*ft_memmove(void *dst, const void *src, size_t len);
 //void		*ft_memchr(const void *s, int c, size_t n);
 //int		ft_memcmp(const void *s1, const void *s2, size_t n);
@@ -51,8 +51,8 @@ char		*ft_strncpy(char *dst, const char *src, size_t len);
 //char		*ft_strrchr(const char *s, int c);
 //char		*ft_strstr(const char *big, const char *little);
 //char		*ft_strnstr(const char*big, const char *little, size_t len);
-//int		ft_strcmp(const char *s1, const char *s2);
-//int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_strcmp(const char *s1, const char *s2);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
 
 // FIRST PART / CHAR //
 
