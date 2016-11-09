b232fb2642951f2ef5f3a87876c7690010006b8e
Arthur GASPARD <agaspard@e1r8p17.42.fr>
2016-11-09 17:26:18 +0100

    strstr

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: agaspard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 17:14:27 by agaspard          #+#    #+#             */
-/*   Updated: 2016/11/08 11:49:19 by agaspard         ###   ########.fr       */
+/*   Updated: 2016/11/09 17:25:47 by agaspard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,21 @@
 
 char	*ft_strstr(const char *big, const char *little)
 {
-	int i;
+	int				i;
+	char			*ptr;
 
 	i = 0;
-	if (*little == '\0')
+	ptr = (char *)big - 1;
+	if (!*little)
 		return ((char*)big);
-	while (*big)
+	while (*(++ptr))
 	{
-		if (*big == little[i])
+		if (*ptr == little[i])
 			i++;
 		else
 			i = 0;
-		if (little[i] == '\0')
-			return ((char*)(big - i + 1));
-		big++;
+		if (!little[i])
+			return (ptr - i + 1);
 	}
 	return (NULL);
 }
